04b25e5b8762ef3bc265bed9b255095a4640249f
Hillary Fan <hillary.fan123@gmail.com>
2019-12-31 23:32:05 -0800

    change background color on scroll

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,6 +5,15 @@ import Navigation from './Navigation.js';
 import About from './About.js';
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.body = React.createRef();
+    this.state = {
+      background: 'sky-bg',
+    }
+    this.handleScroll = this.handleScroll.bind(this)
+  }
+
   componentDidMount() {
     window.addEventListener('scroll', this.handleScroll);
   }
@@ -13,19 +22,21 @@ export default class App extends React.Component {
     window.removeEventListener('scroll', this.handleScroll);
   }
   
-  handleScroll(event) {
-    let scrollTop = event.srcElement.body.scrollTop;
-    if (scrollTop > 50) {
-      console.log('50!');
-      console.log(event.target);
+  handleScroll(e) {
+    if (window.scrollY > 700) {
+      this.setState({background: 'ocean-bg'})
+      // and remove about.css background color
+    } else if (window.scrollY > 1000) {
+      this.setState({background: 'black-bg'})
+    } else if (window.scrollY <= 500) {
+      this.setState({background: 'sky-bg'})
+      // and add about.css background color
     }
-    if (scrollTop < 50) {
-      console.log('<50');
-    }  
   }
+
   render() {
     return (
-      <div>
+      <div className = {this.state.background}>
        <Navigation/>
         <Home/>
         <About/>
