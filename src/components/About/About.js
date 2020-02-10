e33b0063733dda8d27bf1b064c9f476db408393b
Hillary Fan <hillary.fan123@gmail.com>
2020-02-10 13:31:58 -0800

    print response

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -89,6 +89,7 @@ const particlesOptions = {
         if (response === 'Error') {
           console.log('error getting recently played')
         } else {
+          console.log(response);
           var recentTracksResponse = response['recenttracks']['track'];
           var recentTracks = [];
           for (let i = 0; i < 7; i++ ) {
